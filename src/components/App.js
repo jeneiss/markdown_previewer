90c84de69362aaef7fceb5bd454f2789bbef5845
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-08 12:16:43 +0000

    Complete initial functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import marked from 'marked'
+import DOMPurify from 'dompurify'
+
 import './App.css'
 
 function App() {
+  const initialText = 'intial text'
+  const [editorContent, setEditorContent] = useState(initialText)
+  const [previewContent, setPreviewContent] = useState('')
+
+  const handleChange = (e) => {
+    setEditorContent(e.target.value)
+  }
+
+  useEffect(() => {
+    setPreviewContent(marked(editorContent))
+  }, [editorContent])
+
+  const sanitize = () => {
+    const sanitized = DOMPurify.sanitize(previewContent)
+    return {__html: sanitized}
+  }
+
   return (
     <div className="App">
-      {/* MDInput: input text area for markdown */}
-      {/* MDPreview: display are for parsed markdown */}
+
+      <div className='editor__container'>
+        <form>
+          <textarea
+            value={editorContent}
+            onChange={handleChange}
+            id='editor'
+          />
+        </form>
+      </div>
+
+      <div
+        id='preview'
+        dangerouslySetInnerHTML={sanitize()}
+      />
     </div>
   );
 }

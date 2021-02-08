188b72f7f060ba834e1bf070f1055a39728faec9
montresor34 <8246292+montresor34@users.noreply.github.com>
2021-02-08 16:03:56 +0000

    Add initial text and styling

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,30 @@ import DOMPurify from 'dompurify'
 import './App.css'
 
 function App() {
-  const initialText = 'intial text'
+  const initialText = `
+# Welcome to Let There be Markdown
+## The Markdown previewer touched by the hand of God
+In the words of MichelAngelo:
+>I did it for the money
+
+If you need in-depth help, you can find Github's Markdown guide [here](https://guides.github.com/features/mastering-markdown/)
+
+Here's a crash course for the basics:
+
+**Inline code**: \`<div>Hello, world</div>\`
+
+**And this is a code block**:
+\`\`\`
+<div>Hey</div>
+<p>If I point my finger at you, you will become divine</p>
+<p>Hey, come back</div>
+\`\`\`
+**A list of things you can do with this previewer**:
+* Preview Markdown
+* Markdown preview
+* *Achieve divine inspiration through React* ![React logo](./logo192.png)
+`
+
   const [editorContent, setEditorContent] = useState(initialText)
   const [previewContent, setPreviewContent] = useState('')
 
@@ -14,7 +37,7 @@ function App() {
   }
 
   useEffect(() => {
-    setPreviewContent(marked(editorContent))
+    setPreviewContent(marked(editorContent, {breaks: true}))
   }, [editorContent])
 
   const sanitize = () => {
@@ -23,24 +46,35 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className="app">
 
-      <div className='editor__container'>
-        <form>
-          <textarea
-            value={editorContent}
-            onChange={handleChange}
-            id='editor'
+      <div className='content__container'>
+        <div className='title'>
+          <h1>Let there be Markdown</h1>
+        </div>
+
+        <div className='editor'>
+          <form>
+            <textarea
+              value={editorContent}
+              onChange={handleChange}
+              id='editor'
+              className='editor__text-area'
+            />
+          </form>
+        </div>
+
+        <div className='previewer'>
+          <div
+            id='preview'
+            dangerouslySetInnerHTML={sanitize()}
+            className='previewer__text-area'
           />
-        </form>
+        </div>
       </div>
 
-      <div
-        id='preview'
-        dangerouslySetInnerHTML={sanitize()}
-      />
     </div>
-  );
+  )
 }
 
 export default App
